067f55354517d64729be827e755b58c6e879b09a
br1lisboa <brlisbo@gmail.com>
2022-11-14 08:25:09 -0300

    config ES - events - logic

--- src/calendar/pages/CalendarPage.jsx
+++ src/calendar/pages/CalendarPage.jsx
@@ -1,10 +1,60 @@
-import { NavBar } from "../components/NavBar"
+import { Calendar } from 'react-big-calendar'
+import 'react-big-calendar/lib/css/react-big-calendar.css'
+
+
+import { addHours } from "date-fns";
+import { NavBar, CalendarEvent } from "../"
+import { getMessagesES, localizer } from '../../helpers';
+
+
+
+const events = [{
+    title: 'Cumple de Oli',
+    notes: 'Darle un premio',
+    start: new Date(),
+    end: addHours(new Date(), 2),
+    bgColor: '#fafafa',
+    user: {
+        _id: '123',
+        name: 'Bruno'
+    }
+}]
 
 export const CalendarPage = () => {
+
+    const eventStyleGetter = (event, start, end, isSelected) => {
+
+        const style = {
+            backgroundColor: '#347CF7',
+            borderRadios: '0px',
+            opacity: 0.8,
+            color: 'white'
+        }
+
+        return {
+            style
+        }
+
+    }
+
     return (
         <>
-        
+
             <NavBar />
+
+            <Calendar
+                culture='es'
+                localizer={localizer}
+                events={events}
+                startAccessor="start"
+                endAccessor="end"
+                style={{ height: 500 }}
+                messages={getMessagesES()}
+                eventPropGetter={eventStyleGetter}
+                components={{
+                    event: CalendarEvent
+                }}
+            />
 
         </>
     )
