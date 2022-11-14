77e601a012a02cb451aab1cce5413c2f2db75c2e
br1lisboa <brlisbo@gmail.com>
2022-11-14 07:11:16 -0300

    first commit

---
+++ src/router/AppRouter.jsx
@@ -0,0 +1,23 @@
+import { Navigate, Route, Routes } from "react-router-dom"
+import { LoginPage } from "../auth"
+import { CalendarPage } from "../calendar"
+
+export const AppRouter = () => {
+
+    const authStatus = 'not-auth'
+
+    return (
+        <Routes>
+
+            {
+                (authStatus === 'not-auth')
+                    ? <Route path="/auth/*" element={<LoginPage />} />
+                    : <Route path="/*" element={<CalendarPage />} />
+
+            }
+
+            <Route path="/*" element={<Navigate to='/auth/login' />} />
+
+        </Routes>
+    )
+}
