f3c6ec1e41636fdb0ebe48b051d0ba9380221a66
br1lisboa <brlisbo@gmail.com>
2022-11-17 16:40:04 -0300

    fixed some bugs

--- src/helpers/getEnvVariables.js
+++ src/helpers/getEnvVariables.js
@@ -2,10 +2,11 @@
 
 export const getEnvVariables = () => {
 
-    //* ESTA ES LA MANERA EN QUE IMPORTAMOS NUESTRAS VARIABLES DE ENTORNO
-    import.meta.env
+    /* ESTA ES LA MANERA EN QUE IMPORTAMOS NUESTRAS VARIABLES DE ENTORNO
+    import.meta.env */
 
     return {
-        ...import.meta.env
+        //...import.meta.env
+        VITE_API_URL: import.meta.env.VITE_API_URL,
     }
 }
